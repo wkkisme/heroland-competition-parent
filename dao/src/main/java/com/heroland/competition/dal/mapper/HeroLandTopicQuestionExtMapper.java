7947c4a22287e2c77b61530841e634a451839f8a
kai.wangk <wb-wk259992@alibaba-inc.com>
2020-06-18 22:58:26 +0800

    add igion

---
+++ dao/src/main/java/com/heroland/competition/dal/mapper/HeroLandTopicQuestionExtMapper.java
@@ -0,0 +1,11 @@
+package com.heroland.competition.dal.mapper;
+
+import com.heroland.competition.dal.pojo.HeroLandTopicQuestion;
+import com.heroland.competition.dal.pojo.HeroLandTopicQuestionExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface HeroLandTopicQuestionExtMapper extends HeroLandTopicQuestionMapper{
+
+}
