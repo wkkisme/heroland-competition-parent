b3a02aef14901be88d90fe8fc007e369dfb1847a
kai.wangk <wb-wk259992@alibaba-inc.com>
2020-06-20 21:43:37 +0800

    需求

--- dao/src/main/java/com/heroland/competition/dal/mapper/HeroLandCompetitionRecordExtMapper.java
+++ dao/src/main/java/com/heroland/competition/dal/mapper/HeroLandCompetitionRecordExtMapper.java
@@ -1,31 +1,10 @@
 package com.heroland.competition.dal.mapper;
 
-import com.heroland.competition.dal.pojo.HeroLandCompetitionRecord;
 import com.heroland.competition.dal.pojo.HeroLandCompetitionRecordExample;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
-public interface HeroLandCompetitionRecordExtMapper {
-    long countByExample(HeroLandCompetitionRecordExample example);
+public interface HeroLandCompetitionRecordExtMapper extends HeroLandCompetitionRecordMapper {
 
-    int deleteByExample(HeroLandCompetitionRecordExample example);
-
-    int deleteByPrimaryKey(Long id);
-
-    int insert(HeroLandCompetitionRecord record);
-
-    int insertSelective(HeroLandCompetitionRecord record);
-
-    List<HeroLandCompetitionRecord> selectByExample(HeroLandCompetitionRecordExample example);
-
-    HeroLandCompetitionRecord selectByPrimaryKey(Long id);
-
-    int updateByExampleSelective(@Param("record") HeroLandCompetitionRecord record, @Param("example") HeroLandCompetitionRecordExample example);
-
-    int updateByExample(@Param("record") HeroLandCompetitionRecord record, @Param("example") HeroLandCompetitionRecordExample example);
-
-    int updateByPrimaryKeySelective(HeroLandCompetitionRecord record);
-
-    int updateByPrimaryKey(HeroLandCompetitionRecord record);
 }
