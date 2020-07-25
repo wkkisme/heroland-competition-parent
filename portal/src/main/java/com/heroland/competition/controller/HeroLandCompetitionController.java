d5d9dcec81426c4370e9cddf95dcacb3566ab6e2
wk8852lvy@163.com <1258852318Wk>
2020-07-25 08:40:49 +0800

    total score update  to int

--- portal/src/main/java/com/heroland/competition/controller/HeroLandCompetitionController.java
+++ portal/src/main/java/com/heroland/competition/controller/HeroLandCompetitionController.java
@@ -4,6 +4,7 @@ package com.heroland.competition.controller;
 import com.anycommon.response.common.ResponseBody;
 import com.heroland.competition.domain.dp.HeroLandCompetitionRecordDP;
 import com.heroland.competition.service.HeroLandCompetitionService;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +25,7 @@ public class HeroLandCompetitionController {
     private HeroLandCompetitionService heroLandCompetitionService;
 
     @RequestMapping("/doAnswer")
-    public ResponseBody doAnswer(HeroLandCompetitionRecordDP dp) {
+    public ResponseBody doAnswer(@RequestBody HeroLandCompetitionRecordDP dp) {
 
         return heroLandCompetitionService.doAnswer(dp);
     }
--- portal/src/main/java/com/heroland/competition/controller/HeroLandCompetitionStatisticsController.java
+++ portal/src/main/java/com/heroland/competition/controller/HeroLandCompetitionStatisticsController.java
@@ -24,7 +24,9 @@ public class HeroLandCompetitionStatisticsController {
 
 
     /**
-     * 查询同步作业赛列表统计列表
+     * 查询同步作业赛列表统计列表,
+     *  1 查询个人排行榜时或者页面上面的统计信息可根据传userId等信息过来查询
+     *
      *
      * @param qo qo
      * @return HeroLandStatisticsTotalDPs
