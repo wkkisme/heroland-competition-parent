c7a3b34aa0c0eac3c783ad79d63d9029454c4ffa
吴帅苹 <603413965@qq.com>
2020-07-25 18:23:19 +0800

    fix

--- portal/src/main/java/com/heroland/competition/controller/HeroLandCompetitionController.java
+++ portal/src/main/java/com/heroland/competition/controller/HeroLandCompetitionController.java
@@ -4,9 +4,7 @@ package com.heroland.competition.controller;
 import com.anycommon.response.common.ResponseBody;
 import com.heroland.competition.domain.dp.HeroLandCompetitionRecordDP;
 import com.heroland.competition.service.HeroLandCompetitionService;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -24,11 +22,12 @@ public class HeroLandCompetitionController {
     @Resource
     private HeroLandCompetitionService heroLandCompetitionService;
 
-    @RequestMapping("/doAnswer")
+    @PostMapping("/doAnswer")
     public ResponseBody<HeroLandCompetitionRecordDP> doAnswer(@RequestBody HeroLandCompetitionRecordDP dp) {
 
         return heroLandCompetitionService.doAnswer(dp);
     }
 
-
+    @GetMapping("/getSyncCompetitionRecord")
+    public ResponseBody<>
 }
