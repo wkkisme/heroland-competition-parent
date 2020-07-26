8138ec4a5bdab2f94943ef4ca24d7d23c61cf8f3
吴帅苹 <603413965@qq.com>
2020-07-26 16:38:59 +0800

    mock data

--- portal/src/main/java/com/heroland/competition/controller/HeroLandCompetitionStatisticsController.java
+++ portal/src/main/java/com/heroland/competition/controller/HeroLandCompetitionStatisticsController.java
@@ -1,11 +1,13 @@
 package com.heroland.competition.controller;
 
+import cn.hutool.core.collection.ListUtil;
 import com.anycommon.response.common.ResponseBody;
+import com.anycommon.response.utils.ResponseBodyWrapper;
 import com.heroland.competition.common.pageable.PageResponse;
 import com.heroland.competition.domain.dp.AnswerQuestionRecordStatisticDP;
+import com.heroland.competition.domain.dp.CompetitionCourseFinishStatisticDP;
 import com.heroland.competition.domain.dp.HeroLandStatisticsDetailDP;
 import com.heroland.competition.domain.dp.HeroLandStatisticsTotalDP;
-import com.heroland.competition.domain.dp.CompetitionCourseFinishStatisticDP;
 import com.heroland.competition.domain.qo.AnswerQuestionRecordStatisticQO;
 import com.heroland.competition.domain.qo.CourseFinishStatisticQO;
 import com.heroland.competition.domain.qo.HeroLandStatisticsTotalQO;
@@ -16,7 +18,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 比赛统计查询
@@ -161,7 +165,25 @@ public class HeroLandCompetitionStatisticsController {
      */
     @PostMapping("/getCourseFinishStatistic")
     ResponseBody<List<CompetitionCourseFinishStatisticDP>> getCourseFinishStatistic(@RequestBody CourseFinishStatisticQO qo) {
-        return heroLandCompetitionStatisticsService.getCourseFinishStatistic(qo);
+        List<CompetitionCourseFinishStatisticDP> list = new ArrayList<>();
+        AtomicInteger i = new AtomicInteger();
+        ListUtil.toList("中文", "數學", "英文", "常識", "歷史", "電腦", "STEM", "視藝").forEach(s -> {
+            CompetitionCourseFinishStatisticDP dp = new CompetitionCourseFinishStatisticDP();
+            dp.setCourseCode(String.valueOf(i.incrementAndGet()));
+            dp.setCourseName(s);
+            dp.setChapterCount(i.get() + 10);
+            dp.setClassCode("test");
+            dp.setFinishQuestion(i.get());
+            dp.setGradeCode("5");
+            dp.setGradeName("5年級");
+            dp.setOrgCode("1");
+            dp.setQuestionNum(i.get() + 10);
+            dp.setSectionCount(i.get() + 2);
+            dp.setFinishSection(i.get());
+            list.add(dp);
+        });
+        return ResponseBodyWrapper.successWrapper(list);
+//        return heroLandCompetitionStatisticsService.getCourseFinishStatistic(qo);
     }
 
     /**
@@ -172,6 +194,27 @@ public class HeroLandCompetitionStatisticsController {
      */
     @PostMapping("/getAnswerQuestionRecordStatistic")
     PageResponse<AnswerQuestionRecordStatisticDP> getAnswerQuestionRecordStatistic(@RequestBody AnswerQuestionRecordStatisticQO qo) {
-        return heroLandCompetitionStatisticsService.getAnswerQuestionRecordStatistic(qo);
+        List<AnswerQuestionRecordStatisticDP> list = new ArrayList<>();
+        for (int i = 1; i < 33; i++) {
+            AnswerQuestionRecordStatisticDP dp = new AnswerQuestionRecordStatisticDP();
+            dp.setDiff(1);
+            boolean b = i % 2 == 0;
+            dp.setIsCorrectAnswer(b);
+            dp.setKnowledge("四則運算");
+            dp.setLevelCode("2");
+            dp.setOpponentLevel(b ? "同級" : b == (i % 3 == 0) ? "高兩級" : "低一級");
+            dp.setResult(b ? 1 : b == (i % 3 == 0) ? 0 : 2);
+            dp.setScore(b ? 4 : b == (i % 3 == 0) ? 6 : 3);
+            dp.setQuestionId(1L);
+            dp.setTopicName("測試" + i);
+            list.add(dp);
+        }
+        PageResponse<AnswerQuestionRecordStatisticDP> response = new PageResponse<>();
+        response.setItems(list);
+        response.setPage(1);
+        response.setPageSize(40);
+        response.setTotal(33);
+        return response;
+//        return heroLandCompetitionStatisticsService.getAnswerQuestionRecordStatistic(qo);
     }
 }
