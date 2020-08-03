5f4504db905117f6c9798854ec2bad6b90be5e07
sumingjin <sumingjin@youzan.com>
2020-08-03 21:27:23 +0800

    delete

--- service/src/main/java/com/heroland/competition/remote/HeroLandRechargeRemoteServiceImpl.java
+++ service/src/main/java/com/heroland/competition/remote/HeroLandRechargeRemoteServiceImpl.java
@@ -2,18 +2,12 @@ package com.heroland.competition.remote;
 
 import com.anycommon.response.common.ResponseBody;
 import com.heroland.competition.api.HeroLandRechargeRemoteService;
-import com.heroland.competition.common.constants.DiamBizGroupEnum;
-import com.heroland.competition.common.constants.DiamBizTypeEnum;
 import com.heroland.competition.common.utils.NumberUtils;
 import com.heroland.competition.domain.qo.PayOrderQO;
-import com.heroland.competition.domain.request.HerolandDiamRequest;
 import com.heroland.competition.qo.HerolandDiamRechargeQO;
-import com.heroland.competition.service.diamond.HerolandDiamondService;
 import com.heroland.competition.service.order.HerolandOrderService;
-import com.heroland.competition.service.order.HerolandPayService;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.dubbo.config.annotation.Service;
-import org.docx4j.wml.Numbering;
 
 import javax.annotation.Resource;
 
@@ -24,14 +18,10 @@ import javax.annotation.Resource;
 @Service(version = "1.0.0")
 public class HeroLandRechargeRemoteServiceImpl implements HeroLandRechargeRemoteService {
 
-    @Resource
-    private HerolandDiamondService herolandDiamondService;
 
     @Resource
     private HerolandOrderService herolandOrderService;
 
-    @Resource
-    private HerolandPayService herolandPayService;
 
     @Override
     public ResponseBody<Boolean> recharge(HerolandDiamRechargeQO qo) {
