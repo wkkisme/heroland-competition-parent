e4893e5d6c6e94d854d1fcd341e97d688d242638
15085945523 <wk8852lvy@163.com>
2020-08-06 14:22:14 +0800

    班级管理完成

--- portal/src/main/java/com/heroland/competition/controller/HeroLandClassController.java
+++ portal/src/main/java/com/heroland/competition/controller/HeroLandClassController.java
@@ -4,6 +4,7 @@ import com.anycommon.response.common.ResponseBody;
 import com.heroland.competition.domain.dp.HeroLandClassDP;
 import com.heroland.competition.domain.dp.HeroLandUserClassDP;
 import com.heroland.competition.domain.qo.HeroLandClassManageQO;
+import com.heroland.competition.domain.qo.HeroLandUserClassQO;
 import com.heroland.competition.service.classmanage.HeroLandClassService;
 import com.platform.sso.domain.dp.PlatformSysUserClassDP;
 import com.platform.sso.domain.qo.PlatformSysUserClassQO;
@@ -67,6 +68,16 @@ public class HeroLandClassController {
     @RequestMapping(value = "/getClassList")
     public ResponseBody<List<HeroLandUserClassDP>> getClassList(@RequestBody PlatformSysUserClassQO dp) {
         return heroLandClassService.getClassList(dp);
+    }
+
+    /**
+     * 查询老师所在的年级和班级
+     * @param dp
+     * @return
+     */
+    @RequestMapping(value = "/getTeacherInfo")
+    public ResponseBody<List<HeroLandUserClassDP>> getTeacherInfo(@RequestBody HeroLandUserClassQO dp) {
+        return heroLandClassService.getClassUser(dp);
     }
 
 }
