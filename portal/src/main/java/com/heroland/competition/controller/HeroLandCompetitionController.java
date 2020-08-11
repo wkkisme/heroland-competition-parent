dea7e1dcf29224ab19c8d32701e123751600cb60
15085945523 <wk8852lvy@163.com>
2020-08-11 20:26:52 +0800

    youhua

--- portal/src/main/java/com/heroland/competition/controller/HeroLandCompetitionController.java
+++ portal/src/main/java/com/heroland/competition/controller/HeroLandCompetitionController.java
@@ -3,6 +3,8 @@ package com.heroland.competition.controller;
 
 import cn.hutool.core.util.StrUtil;
 import com.anycommon.response.common.ResponseBody;
+import com.anycommon.response.common.UserStatusDP;
+import com.anycommon.response.constant.UserStatusEnum;
 import com.anycommon.response.utils.ResponseBodyWrapper;
 import com.heroland.competition.domain.dp.HeroLandCompetitionRecordDP;
 import com.heroland.competition.domain.qo.HeroLandCompetitionRecordQO;
@@ -10,6 +12,7 @@ import com.heroland.competition.factory.CompetitionTopicFactory;
 import com.heroland.competition.service.HeroLandCompetitionRecordService;
 import com.platform.sso.client.sso.util.CookieUtils;
 import com.platform.sso.facade.PlatformSsoUserServiceFacade;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,6 +37,8 @@ public class HeroLandCompetitionController {
     @Resource
     private PlatformSsoUserServiceFacade platformSsoUserServiceFacade;
 
+    @Resource
+    private RocketMQTemplate rocketMQTemplate;
     @Value("${answer.SyncTime}")
     private Long syncTime;
 
@@ -43,7 +48,21 @@ public class HeroLandCompetitionController {
     @PostMapping("/doAnswer")
     public ResponseBody<HeroLandCompetitionRecordDP> doAnswer(@RequestBody HeroLandCompetitionRecordDP dp, HttpServletRequest request) {
         dp.setUserId(platformSsoUserServiceFacade.queryCurrent(CookieUtils.getSessionId(request)).getData().getUserId());
-        return CompetitionTopicFactory.get(dp.getTopicType()).doAnswer(dp);
+        ResponseBody<HeroLandCompetitionRecordDP> result = CompetitionTopicFactory.get(dp.getTopicType()).doAnswer(dp);
+        try {
+            // 通知所有人 可以被邀请了
+            if (result.isSuccess()) {
+                UserStatusDP userStatusDP = new UserStatusDP();
+                userStatusDP.setTopicId(dp.getTopicId());
+                userStatusDP.setStatus(UserStatusEnum.ONLINE.getStatus());
+                userStatusDP.setUserId(dp.getUserId());
+                userStatusDP.setOrgCode(dp.getOrgCode());
+                userStatusDP.setUserStatus(UserStatusEnum.ONLINE.getStatus());
+                rocketMQTemplate.syncSend("IM_LINE:CLUSTER", userStatusDP.toJSONString());
+            }
+        } catch (Exception ignored) {
+        }
+        return result;
     }
 
 //    @PostMapping("/score/calculate/{userId}")
