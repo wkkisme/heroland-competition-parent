12bc6f4011c2b600620ef82fbe801224a8008f3e
15085945523 <wk8852lvy@163.com>
2020-08-16 14:15:28 +0800

    youhua

--- service/src/main/java/com/heroland/competition/service/impl/HeroLandQuestionServiceImpl.java
+++ service/src/main/java/com/heroland/competition/service/impl/HeroLandQuestionServiceImpl.java
@@ -178,6 +178,7 @@ public class HeroLandQuestionServiceImpl implements HeroLandQuestionService {
             if (qtMap.containsKey(e.getId())) {
                 dto.setAnswer(qtMap.get(e.getId()).get(0).getAnswer());
                 dto.setOptionAnswer(qtMap.get(e.getId()).get(0).getOptionAnswer());
+                dto.setAnalysis(qtMap.get(e.getId()).get(0).getAnalysis());
                 List<QuestionOptionDto> questionOptionDto = JSON.parseArray(qtMap.get(e.getId()).get(0).getOption(), QuestionOptionDto.class);
                 dto.setOptions(questionOptionDto);
             }
