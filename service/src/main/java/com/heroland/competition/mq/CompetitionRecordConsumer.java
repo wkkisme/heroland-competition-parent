6ab01d4924e20edb5f9df4a71ef5a90f3c113db5
15085945523 <wk8852lvy@163.com>
2020-08-23 17:37:43 +0800

    zhengque fix

--- service/src/main/java/com/heroland/competition/mq/CompetitionRecordConsumer.java
+++ service/src/main/java/com/heroland/competition/mq/CompetitionRecordConsumer.java
@@ -1,12 +1,14 @@
 package com.heroland.competition.mq;
 
 import com.alibaba.fastjson.JSON;
+import com.anycommon.cache.service.RedisService;
 import com.anycommon.response.common.ResponseBody;
 import com.heroland.competition.common.enums.CompetitionResultEnum;
 import com.heroland.competition.common.enums.RedisRocketmqConstant;
 import com.heroland.competition.domain.dp.HeroLandCompetitionRecordDP;
 import com.heroland.competition.domain.qo.HeroLandCompetitionRecordQO;
 import com.heroland.competition.service.HeroLandCompetitionRecordService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
@@ -19,6 +21,7 @@ import javax.annotation.Resource;
  *
  * @author mac
  */
+@Slf4j
 @RocketMQMessageListener(topic = "competition-record", consumerGroup = "competition_record_consumer")
 @Component
 public class CompetitionRecordConsumer implements RocketMQListener<HeroLandCompetitionRecordDP> {
@@ -27,19 +30,26 @@ public class CompetitionRecordConsumer implements RocketMQListener<HeroLandCompe
     @Resource
     private HeroLandCompetitionRecordService heroLandCompetitionRecordService;
 
+    @Resource
+    private RedisService redisService;
+
     @Override
     public void onMessage(HeroLandCompetitionRecordDP message) {
+        log.info("比赛记录监听{}",JSON.toJSONString(message));
         HeroLandCompetitionRecordQO heroLandCompetitionRecordQO = new HeroLandCompetitionRecordQO();
         heroLandCompetitionRecordQO.setInviteRecordId(message.getInviteRecordId());
         ResponseBody<HeroLandCompetitionRecordDP> competitionRecordByInviteRecordId = heroLandCompetitionRecordService.getCompetitionRecordByInviteRecordId(heroLandCompetitionRecordQO);
         HeroLandCompetitionRecordDP data = competitionRecordByInviteRecordId.getData();
         if (data != null) {
             if (data.getResult() == null) {
+                String redisKey = data.getTopicId() + data.getInviteId() + data.getOpponentId() +data.getId();
+
                 message.setRecordId(data.getRecordId());
                 message.setId(data.getId());
                 // 如果超时了还是空的 要么是前一个打错，要么是都没答 平局
                 message.setResult(CompetitionResultEnum.DRAW.getResult());
                 heroLandCompetitionRecordService.updateCompetitionRecord(message);
+                redisService.del(redisKey);
             }
             if (data.getInviteEndTime() == null) {
                 message.setSenderId(message.getInviteId());
