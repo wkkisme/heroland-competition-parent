ff878428831e17666c6d816e65ffa33a7f36e471
sumingjin <sumingjin@youzan.com>
2020-09-29 17:21:42 +0800

    fix bug

---
+++ domain/src/main/java/com/heroland/competition/domain/qo/WorldStatisticQO.java
@@ -0,0 +1,22 @@
+package com.heroland.competition.domain.qo;
+
+import com.anycommon.response.common.BaseQO;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+
+/**
+ * @author smjyouzan
+ * @date 2020/9/29
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class WorldStatisticQO extends BaseQO implements Serializable {
+
+    /**
+     * 比赛id
+     */
+    private Long topicId;
+
+}
