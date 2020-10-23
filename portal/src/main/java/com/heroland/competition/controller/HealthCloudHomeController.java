48a369f780cbbf75fcfcf62806d4ba907c73b5d0
wangkai <wk8852lvy@163.com>
2020-10-23 19:33:09 +0800

    fix bugs

--- portal/src/main/java/com/heroland/competition/controller/HealthCloudHomeController.java
+++ portal/src/main/java/com/heroland/competition/controller/HealthCloudHomeController.java
@@ -2,11 +2,14 @@ package com.heroland.competition.controller;
 
 import com.anycommon.cache.service.RedisService;
 //import com.anycommon.logger.annotation.CommonLogger;
+import com.crossoverjie.cim.client.service.ImMessageServerService;
+import com.crossoverjie.cim.client.vo.req.BaseSenderDTO;
 import com.heroland.competition.common.constants.HeroLandRedisConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +20,10 @@ import javax.servlet.http.HttpServletRequest;
 public class HealthCloudHomeController {
 
     private final static Logger logger = LoggerFactory.getLogger(HealthCloudHomeController.class);
+
+
+    @Resource
+    private ImMessageServerService imMessageServerService;
 
     @Resource
     private RedisService redisService;
@@ -38,6 +45,28 @@ public class HealthCloudHomeController {
         return "/res/index.html";
 
     }
+    @RequestMapping(value ={"/tests"} )
+//    @CommonLogger(name = "home 方法")
+    @ResponseBody
+    public Object test(HttpServletRequest request,String sendId,String addId) throws Exception {
+
+        /**
+         * {"inviteId":"2718467022236156294001","opponentId":"2",
+         * "opponentLevel":"ADVERSITY_HERO",
+         *
+         * "opponentStartTime":1596290248665,
+         * "primaryRedisKey":"28427184670222361562940012",
+         * "questionId":"4","topicId":"28","topicName":"wangkai001"}
+         *
+         * topicId + questionId + inviteId + opponentId;
+         */
+        BaseSenderDTO baseSenderDTO = new BaseSenderDTO();
+        baseSenderDTO.setAddresseeId(addId);
+        baseSenderDTO.setSenderId(sendId);
+
+        return imMessageServerService.sendOne2One("sss", baseSenderDTO);
+
+    }
 
 
 //        public static void main(String[] args) {
@@ -47,6 +76,7 @@ public class HealthCloudHomeController {
 //
 //        System.out.println("***************");
 //    }
+
 
 
 }
