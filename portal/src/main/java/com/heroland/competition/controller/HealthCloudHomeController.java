c0cddaa798adbbec21b8906721a8d498f5ef3cf7
wangkai <wk8852lvy@163.com>
2020-10-24 18:03:54 +0800

    fix bugs

--- portal/src/main/java/com/heroland/competition/controller/HealthCloudHomeController.java
+++ portal/src/main/java/com/heroland/competition/controller/HealthCloudHomeController.java
@@ -1,9 +1,6 @@
 package com.heroland.competition.controller;
 
 import com.anycommon.cache.service.RedisService;
-//import com.anycommon.logger.annotation.CommonLogger;
-import com.crossoverjie.cim.client.service.ImMessageServerService;
-import com.crossoverjie.cim.client.vo.req.BaseSenderDTO;
 import com.heroland.competition.common.constants.HeroLandRedisConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,8 +19,6 @@ public class HealthCloudHomeController {
     private final static Logger logger = LoggerFactory.getLogger(HealthCloudHomeController.class);
 
 
-    @Resource
-    private ImMessageServerService imMessageServerService;
 
     @Resource
     private RedisService redisService;
@@ -43,28 +38,6 @@ public class HealthCloudHomeController {
          */
         redisService.del(HeroLandRedisConstants.COMPETITION+"28"+"4"+"2718467022236156294001"+"2");
         return "/res/index.html";
-
-    }
-    @RequestMapping(value ={"/heroland/tests"} )
-//    @CommonLogger(name = "home 方法")
-    @ResponseBody
-    public Object test(HttpServletRequest request,String sendId,String addId) throws Exception {
-
-        /**
-         * {"inviteId":"2718467022236156294001","opponentId":"2",
-         * "opponentLevel":"ADVERSITY_HERO",
-         *
-         * "opponentStartTime":1596290248665,
-         * "primaryRedisKey":"28427184670222361562940012",
-         * "questionId":"4","topicId":"28","topicName":"wangkai001"}
-         *
-         * topicId + questionId + inviteId + opponentId;
-         */
-        BaseSenderDTO baseSenderDTO = new BaseSenderDTO();
-        baseSenderDTO.setAddresseeId(addId);
-        baseSenderDTO.setSenderId(sendId);
-
-        return imMessageServerService.sendOne2One("sss", baseSenderDTO);
 
     }
 
